d0d9d5a9e98e6dd54582b8108c06306722536e3a
StephanyCS1 <stephanycastrosalas@gmail.com>
2023-05-24 11:58:11 -0500

    changes in taste view

--- src/components/tastes/tasteView.jsx
+++ src/components/tastes/tasteView.jsx
@@ -42,7 +42,7 @@ const CategorySelection = () => {
                 <h1>¿Cuales son tus gustos culinarios?</h1>
             </div>
 
-            <div className="flex w-11/12 justify-around items-center text-2xl font-medium py-3 my-3 mx-auto">
+            <div className="flex w-11/12 text-center items-center text-2xl font-medium py-3 my-3 mx-auto">
                 <h3 >Para ofrecerte recomendaciones personalizadas, cuéntanos tus comidas favoritas y si tienes alguna restricción alimentaria o preferencia especial. </h3>
             </div>
             <div>
@@ -61,19 +61,19 @@ const CategorySelection = () => {
                     pagination={{el: '.pagination', clickable: true}}
                 >
                     <div className="grid grid-cols-1 mx-auto sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 mx-4 sm:mx-8 md:mx-12 lg:mx-40 xl:mx-52">
-                    {
-                        tasteJSON.map(item => (
-                            <SwiperSlide key={item.id} spaceBetween={20}>
-                                <div
-                                    key={item.id}
-                                    className={`${selectedCategories.includes(item.name) ? 'selected ' : 'grayscale'}`}
-                                    onClick={() => handleCategoryChange(item.name)}
-                                >
-                                <CardTastes name={item.name} img={item.img} />
-                                </div>
-                            </SwiperSlide>
-                        ))
-                    }
+                        {
+                            tasteJSON.map(item => (
+                                <SwiperSlide key={item.id} spaceBetween={20}>
+                                    <div
+                                        key={item.id}
+                                        className={`${selectedCategories.includes(item.name) ? 'selected ' : 'grayscale'}`}
+                                        onClick={() => handleCategoryChange(item.name)}
+                                    >
+                                        <CardTastes name={item.name} img={item.img} />
+                                    </div>
+                                </SwiperSlide>
+                            ))
+                        }
                     </div>
                 </Swiper>
                 {/* Buttons Slide */}
